c9e11a4611caf0e4731c26ce86da83c0a545eee7
Ech-Chouini Mehdi <mehdi.ech@student-cs.fr>
2021-11-24 04:44:31 +0100

    producer main file

---
+++ Tweet_Collector/producer.cpp
@@ -0,0 +1,14 @@
+#include "tweetoscopeCollectorParams.hpp"
+#include "tweet.hpp"
+int main(int argc, char *argv[])
+{
+
+    auto key = std::to_string(int_key);
+    builder.key(key);
+    std::ostringstream ostr;
+    ostr << ... the JSON message auto msg = ostr.str();
+    builder.payload(msg);
+    producer.produce(builder);
+
+    return 0;
+}
