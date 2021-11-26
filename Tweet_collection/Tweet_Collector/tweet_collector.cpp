93559a0580ae3e48e00c24ed2e6ff4dbd97bbe18
Ech-Chouini Mehdi <mehdi.ech@student-cs.fr>
2021-11-26 16:02:22 +0100

    added consumer and producer in collector wip

--- Tweet_collection/Tweet_Collector/tweet_collector.cpp
+++ Tweet_collection/Tweet_Collector/tweet_collector.cpp
@@ -1,6 +1,6 @@
 #include "tweetoscopeCollectorParams.hpp"
 #include <cppkafka/cppkafka.h>
-
+#include "tweet.hpp"
 int main(int argc, char *argv[])
 {
 
@@ -14,7 +14,51 @@ int main(int argc, char *argv[])
               << "Parameters : " << std::endl
               << "----------" << std::endl
               << std::endl
-              << params.kafka.brokers << std::endl
+              << params << std::endl
               << std::endl;
-    return 0;
-}
+
+    cppkafka::Configuration cons_config = {
+        {"metadata.broker.list", params.kafka.brokers},
+
+    };
+    cppkafka::Consumer consumer(cons_config);
+    consumer.subscribe({params.topic.in});
+    cppkafka::Configuration prod_config = {
+        {"metadata.broker.list", params.kafka.brokers}};
+
+    // Create the producer
+    cppkafka::Producer producer(prod_config);
+    //auto int_key = MessageBuilder(params.topic.in).partition(0);
+    //auto key = std::to_string(int_key);
+    //MessageBuilder builder = MessageBuilder(params.topic.in).partition(0);
+    cppkafka::MessageBuilder PartialMessageBuilder{params.topic.out_series};
+    //builder.key(key);
+    // Implementation of a Producer which write on terminated Cascades : properties
+    cppkafka::MessageBuilder TerminatedMessageBuilder{params.topic.out_properties};
+    tweetoscope::params::section::Times time;
+    time.observation = params.times.observation;
+    time.terminated = params.times.terminated;
+
+    auto msg = consumer.poll();
+
+    // Assert msg is not empty and there no errors
+    if (msg && !msg.get_error())
+    {
+        // Instanciation of a tweet
+        tweetoscope::tweet Twt;
+        auto init_key = tweetoscope::cascade::idf(std::stoi(msg.get_key()));
+        auto istr = std::istringstream(std::string(msg.get_payload()));
+        istr >> Twt;
+
+        //  Creating processor of the source if not already created
+        auto key = std::to_string(init_key);
+
+        if (Twt.type == "tweet")
+        {
+        }
+        else
+        {
+        }
+
+        return 0;
+    }
