440fd05ec146e9ddad8f2258cdc26a9e7581ae1b
Ech-Chouini Mehdi <mehdi.ech@student-cs.fr>
2021-12-02 21:14:45 +0100

    first try for refactoring

--- Pipeline_ai/predictor.py
+++ Pipeline_ai/predictor.py
@@ -11,9 +11,95 @@ import logger
 
 from Hawks_processes.Models.predict import HawksProcess
 
+alpha, mu = 2.4, 10
+
+
 parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
 parser.add_argument("--broker-list", type=str, required=True, help="the broker list")
 args = parser.parse_args()  # Parse arguments
+
+
+def calculate_w_true(n_obs, n_tot, n_star, G1, estimator):
+    n = estimator.prediction_one_shot(n_obs)
+    try:
+        w_true = (n_tot - n) * (1 - n_star) / G1
+    except ZeroDivisionError:
+        w_true = -1
+    return w_true
+
+
+def get_values(
+    msg,
+    topic_model="cascade_model",
+    w_true=None,
+    cid_params_dict={},
+    cid_n_tot_dict={},
+    dict_model={"300": None, "600": None, "1200": None},
+):
+    msg_value = msg.value
+    T_obs = msg.key
+    cid = None
+    if msg.topic == topic_model:
+        logger.debug("Received a model!")
+        logger.debug("model for observation " + msg.key)
+        model = msg.value
+        dict_model[msg.key] = model
+
+    else:
+        if msg_value["type"] == "parameters":
+
+            # { 'type': 'parameters', 'cid': 'tw23981', 'msg' : 'blah blah', 'n_obs': 32, 'n_supp' : 120, 'params': [ 0.0423, 124.312 ], n_star G1 }
+            # Getting data from msg
+            params = msg_value["params"]
+            n_obs = msg_value["n_obs"]
+            cid = msg_value["cid"]
+            n_star = msg_value["n_star"]
+            G1 = msg_value["G1"]
+
+            if (cid, T_obs) not in cid_n_tot_dict.keys():
+                cid_params_dict[(cid, T_obs)] = msg_value
+            else:
+                n_tot = cid_n_tot_dict[(cid, T_obs)]
+                estimator = HawksProcess(
+                    alpha=alpha, mu=mu, n_star=n_star, params=params, G1=G1
+                )
+                w_true = calculate_w_true(n_obs, n_tot, n_star, G1, estimator)
+
+        elif msg_value["type"] == "size":
+
+            # { 'type' : 'size', 'cid': 'tw23981', 'n_tot': 127, 't_end': 4329 }
+            cid = msg_value["cid"]
+            n_tot = msg_value["n_tot"]
+
+            if (cid, T_obs) not in cid_params_dict.keys():
+                cid_n_tot_dict[(cid, T_obs)] = n_tot
+
+            else:
+
+                msg_params = cid_params_dict[(cid, T_obs)]
+                n_star = msg_params["n_star"]
+                G1 = msg_params["G1"]
+                params = msg_params["params"]
+                n_obs = msg_params["n_obs"]
+                estimator = HawksProcess(
+                    alpha=alpha, mu=mu, n_star=n_star, params=params, G1=G1
+                )
+                w_true = calculate_w_true(n_obs, n_tot, n_star, G1, estimator)
+
+        return w_true, cid, T_obs
+
+
+def get_params(cid_params_dict, cid, T_obs):
+    params, G1, n_star, n_obs = None, None, None, None
+
+    if (cid, T_obs) in cid_params_dict.keys():
+        msg_params = cid_params_dict[(cid, T_obs)]
+        n_star = msg_params["n_star"]
+        G1 = msg_params["G1"]
+        params = msg_params["params"]
+        n_obs = msg_params["n_obs"]
+
+    return params, G1, n_star, n_obs
 
 
 def deserializer(
@@ -24,15 +110,6 @@ def deserializer(
     except UnicodeDecodeError:
         return pickle.loads(v)
 
-
-consumer_properties = KafkaConsumer(
-    "cascade_properties",  # Topic name
-    bootstrap_servers=args.broker_list,  # List of brokers passed from the command line
-    value_deserializer=lambda v: json.loads(
-        v.decode("utf-8")
-    ),  # How to deserialize the value from a binary buffer
-    key_deserializer=lambda v: v.decode(),  # How to deserialize the key (if any)
-)
 
 consumer = KafkaConsumer(
     bootstrap_servers=args.broker_list,
@@ -62,72 +139,16 @@ n_star = None
 
 
 for msg in consumer:
-    alpha, mu = 2.4, 10
-    msg_value = msg.value
-    w_true = None
 
-    if msg.topic == "cascade_model":
-        logger.debug("Received a model!")
-        logger.debug("model for observation " + msg.key)
-        model = msg.value
-        dict_model[msg.key] = model
+    w_true, cid, T_obs, n_obs = get_values(
+        msg,
+        topic_model="cascade_model",
+        w_true=None,
+        cid_params_dict=cid_params_dict,
+        cid_n_tot_dict=cid_n_tot_dict,
+    )
 
-    else:
-        if msg_value["type"] == "parameters":
-
-            # { 'type': 'parameters', 'cid': 'tw23981', 'msg' : 'blah blah', 'n_obs': 32, 'n_supp' : 120, 'params': [ 0.0423, 124.312 ], n_star G1 }
-            # Getting data from msg
-            n_supp = msg_value["n_supp"]
-            params = msg_value["params"]
-            n_obs = msg_value["n_obs"]
-            cid = msg_value["cid"]
-            n_star = msg_value["n_star"]
-            G1 = msg_value["G1"]
-            key = msg.key
-
-            if (cid, key) not in cid_n_tot_dict.keys():
-                cid_params_dict[(cid, key)] = msg_value
-            else:
-                n_tot = cid_n_tot_dict[(cid, key)]
-                estimator = HawksProcess(
-                    alpha=alpha, mu=mu, n_star=n_star, params=params, G1=G1
-                )
-                n = estimator.prediction_one_shot(n_obs)
-                try:
-                    w_true = (n_tot - n) * (1 - n_star) / G1
-                except ZeroDivisionError:
-                    w_true = -1
-
-            T_obs = msg.key
-
-        elif msg_value["type"] == "size":
-
-            # { 'type' : 'size', 'cid': 'tw23981', 'n_tot': 127, 't_end': 4329 }
-            cid = msg_value["cid"]
-            n_tot = msg_value["n_tot"]
-
-            t_end = msg_value["t_end"]
-            T_obs = msg.key
-            if (cid, T_obs) not in cid_params_dict.keys():
-                cid_n_tot_dict[(cid, T_obs)] = n_tot
-
-            else:
-
-                msg_params = cid_params_dict[(cid, key)]
-                n_star = msg_params["n_star"]
-                G1 = msg_params["G1"]
-                params = msg_params["params"]
-                estimator = HawksProcess(
-                    alpha=alpha, mu=mu, n_star=n_star, params=params, G1=G1
-                )
-                n = estimator.prediction_one_shot(n_obs)
-                try:
-                    w_true = (n_tot - n) * (1 - n_star) / G1
-                except ZeroDivisionError:
-                    w_true = -1
-        else:
-            continue
-
+    params, G1, n_star = get_params(cid_params_dict, cid, T_obs)
     if w_true:
         key = T_obs
         value_sample = {
@@ -136,8 +157,10 @@ for msg in consumer:
             "X": [params[1], G1, n_star],
             "W": w_true,
         }
-
         producer.send("cascade_samples", key=T_obs, value=value_sample)
+    else:
+        continue
+
     if n_star is not None:
         estimator = HawksProcess(
             alpha=alpha, mu=mu, n_star=n_star, params=params, G1=G1
@@ -146,11 +169,8 @@ for msg in consumer:
         if dict_model[msg.key]:
             model = dict_model[msg.key]
 
-        # w_model = model.predict([[params[1], G1, n_star]])
-        # n_model = n + w_model[0] * (G1 / (1 - n_star))
         n_model = estimator.prediction_one_shot(n, model)
 
-        T_obs = msg.key
         if n_model > 150:
             # Key = None Value = { 'type': 'alert', 'cid': 'tw23981', 'msg' : 'blah blah', 'T_obs': 600, 'n_tot' : 158 }
             alert_value = {
@@ -173,8 +193,8 @@ for msg in consumer:
                 "======================================="
                 "stat" + "======================================="
             )
-            are = np.abs(n_model - n_tot) / n_tot
 
+            are = np.abs(n_model - n_tot) / n_tot
             # Key = None Value = { 'type': 'stat', 'cid': 'tw23981', 'T_obs': 600, 'ARE' : 0.93 }
             stat_value = {
                 "type": "stat",
@@ -185,15 +205,8 @@ for msg in consumer:
             logger.info(
                 "=====================================New insight!========================="
             )
-            logger.debug(
-                "true   "
-                + str(n_tot)
-                + "   predicted  "
-                + str(n_model)
-                + "   Tobs "
-                + str(T_obs)
-            )
-            logger.debug("ARE value " + str(are))
+
+            logger.info("ARE value :" + str(are))
             logger.info("==============================================")
 
             producer.send("cascade_stat", key=T_obs, value=stat_value)
