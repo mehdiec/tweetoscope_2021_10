0ce2045c8cd57a0208d87e2ff1d4c27a7851272a
Ech-Chouini Mehdi <mehdi.ech@student-cs.fr>
2021-12-02 23:45:41 +0100

    turned functions into methods

--- Pipeline_ai/predictor.py
+++ Pipeline_ai/predictor.py
@@ -1,119 +1,26 @@
 import argparse  # To parse command line arguments
 import json  # To parse and dump JSON
 import numpy as np
-import pickle
-import sklearn
 from kafka import KafkaConsumer  # Import Kafka consumer
 from kafka import KafkaProducer  # Import Kafka producer
 from sklearn.ensemble import RandomForestRegressor
+from util import deserializer
+from utils.predictor_utils import Predictor
 import logger
 
 
 from Hawks_processes.Models.predict import HawksProcess
 
 alpha, mu = 2.4, 10
+cid = None
+samples = []
+n_star = None
+w_true_old = np.inf
 
 
 parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
 parser.add_argument("--broker-list", type=str, required=True, help="the broker list")
 args = parser.parse_args()  # Parse arguments
-
-
-def calculate_w_true(n_obs, n_tot, n_star, G1, estimator):
-    n = estimator.prediction_one_shot(n_obs)
-    try:
-        w_true = (n_tot - n) * (1 - n_star) / G1
-    except ZeroDivisionError:
-        w_true = -1
-    return w_true
-
-
-def get_model(msg, dict_model={"300": None, "600": None, "1200": None}):
-
-    T_obs = msg.key
-    if msg.topic == "cascade_model":
-        logger.debug("Received a model!")
-        logger.debug("model for observation " + msg.key)
-        model = msg.value
-        dict_model[T_obs] = model
-
-
-def get_values(
-    msg,
-    topic_model="cascade_model",
-    w_true=None,
-    cid_params_dict={},
-    cid_n_tot_dict={},
-):
-
-    msg_value = msg.value
-    T_obs = msg.key
-    cid = None
-    if msg.topic != topic_model:
-        if msg_value["type"] == "parameters":
-
-            # { 'type': 'parameters', 'cid': 'tw23981', 'msg' : 'blah blah', 'n_obs': 32, 'n_supp' : 120, 'params': [ 0.0423, 124.312 ], n_star G1 }
-            # Getting data from msg
-            params = msg_value["params"]
-            n_obs = msg_value["n_obs"]
-            cid = msg_value["cid"]
-            n_star = msg_value["n_star"]
-            G1 = msg_value["G1"]
-            cid_params_dict[(cid, T_obs)] = msg_value
-
-            if (cid, T_obs) in cid_n_tot_dict.keys():
-
-                n_tot = cid_n_tot_dict[(cid, T_obs)]
-                estimator = HawksProcess(
-                    alpha=alpha, mu=mu, n_star=n_star, params=params, G1=G1
-                )
-                w_true = calculate_w_true(n_obs, n_tot, n_star, G1, estimator)
-
-        elif msg_value["type"] == "size":
-
-            # { 'type' : 'size', 'cid': 'tw23981', 'n_tot': 127, 't_end': 4329 }
-            cid = msg_value["cid"]
-            n_tot = msg_value["n_tot"]
-            cid_n_tot_dict[(cid, T_obs)] = n_tot
-            if (cid, T_obs) in cid_params_dict.keys():
-
-                msg_params = cid_params_dict[(cid, T_obs)]
-                n_star = msg_params["n_star"]
-                G1 = msg_params["G1"]
-                params = msg_params["params"]
-                n_obs = msg_params["n_obs"]
-                estimator = HawksProcess(
-                    alpha=alpha, mu=mu, n_star=n_star, params=params, G1=G1
-                )
-                w_true = calculate_w_true(n_obs, n_tot, n_star, G1, estimator)
-        if cid:
-            return [w_true, cid, T_obs]
-        else:
-            return []
-
-
-def get_params(cid_params_dict, cid_n_tot_dict, cid, T_obs):
-    params, G1, n_star, n_obs, n_tot = None, None, None, None, None
-
-    if (cid, T_obs) in cid_params_dict.keys():
-        msg_params = cid_params_dict[(cid, T_obs)]
-        n_star = msg_params["n_star"]
-        G1 = msg_params["G1"]
-        params = msg_params["params"]
-        n_obs = msg_params["n_obs"]
-    if (cid, T_obs) in cid_n_tot_dict.keys():
-        n_tot = cid_n_tot_dict[(cid, T_obs)]
-
-    return params, G1, n_star, n_obs, n_tot
-
-
-def deserializer(
-    v,
-):  # the deserializer depends on the topic. If it's not json, it's pickle
-    try:
-        return json.loads(v.decode("utf-8"))
-    except UnicodeDecodeError:
-        return pickle.loads(v)
 
 
 consumer = KafkaConsumer(
@@ -135,33 +42,23 @@ producer = KafkaProducer(
 logger = logger.get_logger("predictor", broker_list=args.broker_list, debug=True)
 consumer.subscribe(["cascade_properties", "cascade_model"])
 
-cid_n_tot_dict = {}  # dictionnary with cid as keys and n_tot as values
-cid_params_dict = {}  # dictionary with cid as keys and params as value
-dict_model = {"300": None, "600": None, "1200": None}
-model = None
-n_tot = None
-n_star = None
-cid = None
-samples = []
-w_true_old = np.inf
+predictor = Predictor()
+
+
 for msg in consumer:
 
-    get_model(msg, dict_model)
+    predictor.get_model(msg)
 
-    values = get_values(
+    values = predictor.get_values(
         msg,
         topic_model="cascade_model",
-        w_true=None,
-        cid_params_dict=cid_params_dict,
-        cid_n_tot_dict=cid_n_tot_dict,
     )
 
     if values:
         w_true, cid, T_obs = values
+
     if cid:
-        params, G1, n_star, n_obs, n_tot = get_params(
-            cid_params_dict, cid_n_tot_dict, cid, T_obs
-        )
+        params, G1, n_star, n_obs, n_tot = predictor.get_params(cid, T_obs)
 
         if w_true and w_true != w_true_old:
             w_true_old = w_true
@@ -183,8 +80,7 @@ for msg in consumer:
             alpha=alpha, mu=mu, n_star=n_star, params=params, G1=G1
         )
         n = estimator.prediction_one_shot(n_obs)
-        if dict_model[msg.key]:
-            model = dict_model[msg.key]
+        model = predictor.dict_model[msg.key]
 
         n_model = estimator.prediction_one_shot(n, model)
 
@@ -228,7 +124,7 @@ for msg in consumer:
 
             producer.send("cascade_stat", key=T_obs, value=stat_value)
 
-            cid_n_tot_dict.pop((cid, T_obs), None)
+            predictor.cid_n_tot_dict.pop((cid, T_obs), None)
 
 
 producer.flush()
